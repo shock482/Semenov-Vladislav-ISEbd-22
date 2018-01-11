04966c21c2f20fe3cc77bb434429285a70ee9fe4
shock482 <shock482@gmail.com>
2018-01-11 20:37:27 +0400

    Update ClassArray.cs

--- ClassArray.cs
+++ ClassArray.cs
@@ -11,68 +11,6 @@ namespace lab2sem1
 
     class ClassArray<T> : IEnumerator<T>, IEnumerable<T>, IComparable<ClassArray<T>>
     {
-        /* private T[] places;
-         private T defaultValue;
-
-         public ClassArray(int sizes, T defVal)
-         {
-             defaultValue = defVal;
-             places = new T[sizes];
-             for (int i = 0; i < places.Length; i++)
-             {
-                 places[i] = defaultValue;
-             }
-         }
-
-         public T getObject(int ind)
-         {
-             if (ind > -1 && ind < places.Length)
-             {
-                 return places[ind];
-             }
-             return defaultValue;
-         }
-
-         public static int operator +(ClassArray<T> p, T rock)
-         {
-             for (int i = 0; i < p.places.Length; i++)
-             {
-                 if (p.CheckFreePlace(i))
-                 {
-                     p.places[i] = rock;
-                     return i;
-                 }
-             }
-             return -1;
-         }
-
-         public static T operator -(ClassArray<T> p, int index)
-         {
-             if (!p.CheckFreePlace(index))
-             {
-                 T rock = p.places[index];
-                 p.places[index] = p.defaultValue;
-                 return rock;
-             }
-             return p.defaultValue;
-         }
-
-         private bool CheckFreePlace(int index)
-         {
-             if (index < 0 || index > places.Length)
-             {
-                 return false;
-             }
-             if (places[index] == null)
-             {
-                 return true;
-             }
-             if (places[index].Equals(defaultValue))
-             {
-                 return true;
-             }
-             return false;
-         }*/
 
         private Dictionary<int, T> places;
         private int maxCount;
@@ -84,13 +22,6 @@ namespace lab2sem1
             places = new Dictionary<int, T>();
             maxCount = 15;
         }
-
-       /* class ParkingOverflowException : Exception
-        {
-            public ParkingOverflowException() :
-                base("В магазине нет свободных мест")
-            { }
-        }*/
 
         public static int operator +(ClassArray<T> p, T rock)
         {
@@ -210,14 +141,6 @@ namespace lab2sem1
             }
             return 0;
         }
-
-/* class ParkingIndexOutOfRangeException : Exception
- {
-     public ParkingIndexOutOfRangeException() :
-         base("В магазине нет драгоценности по такому индексу")
-     { }
- }*/
-
 public static T operator -(ClassArray<T> p, int index)
         {
             if (p.places.ContainsKey(index))
@@ -226,7 +149,6 @@ public static T operator -(ClassArray<T> p, int index)
                 p.places.Remove(index);
                 return rock;
             }
-            // return p.defaultValue;
             throw new ParkingIndexOutOfRangeException();
         }
 
