c5f3167258ee0375708e0d6661ab766c6e7d0ea0
shock482 <shock482@gmail.com>
2018-01-11 20:45:25 +0400

    Update ClassArray.cs

--- ClassArray.cs
+++ ClassArray.cs
@@ -9,69 +9,6 @@ namespace laba2sem1
     
     class ClassArray <T> where T : IRock
     {
-        /* private T[] places;
-         private T defaultValue;
-
-         public ClassArray(int sizes, T defVal)
-         {
-             defaultValue = defVal;
-             places = new T[sizes];
-             for (int i = 0; i < places.Length; i++)
-             {
-                 places[i] = defaultValue;
-             }
-         }
-
-         public T getObject(int ind)
-         {
-             if (ind > -1 && ind < places.Length)
-             {
-                 return places[ind];
-             }
-             return defaultValue;
-         }
-
-         public static int operator +(ClassArray<T> p, T rock)
-         {
-             for (int i = 0; i < p.places.Length; i++)
-             {
-                 if (p.CheckFreePlace(i))
-                 {
-                     p.places[i] = rock;
-                     return i;
-                 }
-             }
-             return -1;
-         }
-
-         public static T operator -(ClassArray<T> p, int index)
-         {
-             if (!p.CheckFreePlace(index))
-             {
-                 T rock = p.places[index];
-                 p.places[index] = p.defaultValue;
-                 return rock;
-             }
-             return p.defaultValue;
-         }
-
-         private bool CheckFreePlace(int index)
-         {
-             if (index < 0 || index > places.Length)
-             {
-                 return false;
-             }
-             if (places[index] == null)
-             {
-                 return true;
-             }
-             if (places[index].Equals(defaultValue))
-             {
-                 return true;
-             }
-             return false;
-         }*/
-
         private Dictionary<int, T> places;
         private int maxCount;
         private T defaultValue;
